9780c0ab14526670a8f15344eb0ed916d36fef51
Mario Albert <Mario.Albert@gmx.de>
2014-10-13 09:14:42 +0200

    improve again spec for issue 347

--- spec/line_wrap_spec.rb
+++ spec/line_wrap_spec.rb
@@ -167,18 +167,7 @@ describe "Core::Text::Formatted::LineWrap#wrap_line" do
                                   :width => 300,
                                   :document => @pdf)
     string.should == "helloworld hiearth"
-
-    string1 = "hello#{Prawn::Text::SHY}world "
-    string2 = @pdf.font.normalize_encoding("hi#{Prawn::Text::SHY}earth")
-    array = [{ :text => string1 }, { :text => string2 }]
-    @arranger.format_array = array
-    string = @line_wrap.wrap_line(:arranger => @arranger,
-                                  :width => 300,
-                                  :document => @pdf)
-    string.should == "helloworld hiearth"
   end
-
-  it 'should not display soft hyphens except at the end of a line'
 
   it "should not break before a hard hyphen that follows a word" do
     enough_width_for_hello_world = 60
